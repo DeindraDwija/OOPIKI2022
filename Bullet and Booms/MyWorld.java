6b8c105a0390f9505af285cc40dda131dc4d3d97
I Gede Deindra Dwija Puridiasta <114994571+DeindraDwija@users.noreply.github.com>
2022-10-12 20:59:43 +0800

    Add files via upload

---
+++ Bullet and Booms/MyWorld.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        addhero();
+        act();
+    }
+    
+    public void addhero(){
+        Hero hero = new Hero();
+        addObject(hero,50,200);
+    }
+    
+    int enemyCount = 0;
+    public void act(){
+        enemyCount++;
+        if (enemyCount==120){
+            addenemy();
+            enemyCount = 0;
+        }
+    }
+
+    public void addenemy(){
+        int enemyY = Greenfoot.getRandomNumber(getHeight());
+        Enemy enemy = new Enemy(Greenfoot.getRandomNumber(4) + 1);
+        addObject(enemy,600,enemyY);
+    }
+}
